45c34cf5e137c6ead0eb425268627ab0fc7e1e54
Emehelu Amobi David <dprobityconcept@gmail.com>
2025-07-12 10:45:44 -0500

    task 2

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,28 @@
+from seed import connect_to_prodev
+
+def stream_users_in_batches(batch_size):
+    connection = connect_to_prodev()
+    if not connection:
+        return
+    
+
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM  user_data")
+
+    while True:
+        batch  = cursor.fetchmany(size=batch_size)
+        if not batch:
+            break
+        yield batch 
+
+    cursor.close()
+    connection.close()
+
+
+
+
+def batch_processing(batch_size):
+    for batch in stream_users_in_batches(batch_size):
+        for user in batch:
+            if float(user["age"]) > 25:    # Cast to float in case age is Decimal
+                print(user)
