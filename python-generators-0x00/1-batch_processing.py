2aad5cffb369302fe3d7d7b03b2827b54009c2cc
Emehelu Amobi David <dprobityconcept@gmail.com>
2025-07-12 20:28:35 -0500

    solved the 1-batch_processing.py doesn't contain: [FROM user_data] error

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -1,28 +1,83 @@
-from seed import connect_to_prodev
+import mysql.connector
+
+
+def connect_to_prodev():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Qwertykey1!@",
+        database="ALX_prodev"
+            
+    )
+
+
 
 def stream_users_in_batches(batch_size):
     connection = connect_to_prodev()
+
     if not connection:
         return
-    
-
     cursor = connection.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM  user_data")
+    cursor.execute("SELECT * from user_data")
 
     while True:
-        batch  = cursor.fetchmany(size=batch_size)
+        batch = cursor.fetchmany(size=batch_size)
         if not batch:
             break
-        yield batch 
+        yield batch
 
     cursor.close()
     connection.close()
 
 
-
-
 def batch_processing(batch_size):
     for batch in stream_users_in_batches(batch_size):
         for user in batch:
-            if float(user["age"]) > 25:    # Cast to float in case age is Decimal
+            if float(user["age"]) > 25:
                 print(user)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from seed import connect_to_prodev
+
+# def stream_users_in_batches(batch_size):
+#     connection = connect_to_prodev()
+#     if not connection:
+#         return
+    
+
+#     cursor = connection.cursor(dictionary=True)
+#     cursor.execute("SELECT * FROM  user_data")
+
+#     while True:
+#         batch  = cursor.fetchmany(size=batch_size)
+#         if not batch:
+#             break
+#         yield batch 
+
+#     cursor.close()
+#     connection.close()
+
+
+
+
+# def batch_processing(batch_size):
+#     for batch in stream_users_in_batches(batch_size):
+#         for user in batch:
+#             if float(user["age"]) > 25:    # Cast to float in case age is Decimal
+#                 print(user)
