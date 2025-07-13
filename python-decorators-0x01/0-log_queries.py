2fa29c7326113621bb8aed552ba011e8ec2522c9
Emehelu Amobi David <dprobityconcept@gmail.com>
2025-07-13 18:04:05 -0500

    track which query was passed at function run

---
+++ python-decorators-0x01/0-log_queries.py
@@ -0,0 +1,25 @@
+import sqlite3
+import functools
+
+#### decorator to log SQ\l queries
+
+def log_queries(func):
+    @functools.wraps(func)
+    def wrapper(query, *args, **kwargs):
+        print(f"[LOG] Executing SQL Query: {kwargs.get('query')}")
+        return func(*args, **kwargs)
+    return wrapper
+
+
+
+@log_queries
+def fetch_all_users(qwery):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(qwery)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+#### fetch users while logging the query 
+users = fetch_all_users(qwery="SELECT * FROM users")
