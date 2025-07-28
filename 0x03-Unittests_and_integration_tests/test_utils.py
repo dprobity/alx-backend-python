7939cceeb5f8e7c7c73a9052d6e5a90730977bc0
Emehelu Amobi David <dprobityconcept@gmail.com>
2025-07-28 09:38:33 -0500

    checking exception raise

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -17,5 +17,16 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
+     # New Task 1 test
+    @parameterized.expand([
+        ({}, ("a",), 'a'),
+        ({"a": 1}, ("a", "b"), 'b'),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected_key):
+        """Test that KeyError is raised for missing keys"""
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), f"'{expected_key}'")
+
 if __name__ == "__main__":
     unittest.main()
