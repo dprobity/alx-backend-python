3de9e33b656d214e26c610f4b63e8a23559d9cad
Emehelu Amobi David <dprobityconcept@gmail.com>
2025-08-01 00:22:21 -0500

    added the permision and athentication settings

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -41,6 +41,23 @@ INSTALLED_APPS = [
     "rest_framework",
 ]
 
+
+
+AUTH_USER_MODEL = 'chats.User'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+}
+
+
+
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
