95481bc75f0957c0d95f699f254332cd30237718
Emehelu Amobi David <dprobityconcept@gmail.com>
2025-08-02 22:29:19 -0500

    solved the model migration issue related to using AbstractUser vs AbstractBaseUser class

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,46 @@
-from django.db import models
+import uuid
+from django.contrib.auth.models import AbstractUser
+from django.db import models 
 
-# Create your models here.
+
+
+
+
+class User(AbstractUser):
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    ROLE_CHOICES = [
+        ('guest', 'Guest'),
+        ('host', 'Host'),
+        ('admin', 'Admin'),
+    ]
+
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='guest')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    REQUIRED_FIELDS = ['email', 'first_name', 'last_name']
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
+    
+
+class Conversation(models.Model):
+    conversation_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    participants = models.ManyToManyField('User', related_name='conversations')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        participants = ','.join([p.username for p in self.participants.all()])
+        return f"Conversation {self.conversation_id} [{participants}]"
+    
+
+
+class Message(models.Model):
+    message_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    sender = models.ForeignKey('User', on_delete=models.CASCADE, related_name='messages')
+    conversation = models.ForeignKey('Conversation', on_delete=models.CASCADE, related_name='messages')
+    message_body = models.TextField()
+    sent_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.sender.username}: {self.message_body[:30]}"
